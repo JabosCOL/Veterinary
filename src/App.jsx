033034901e94ae8393005f43e1b10c15eb1ccced
JabosCOL <cesar.bolivarc@hotmail.com>
2022-09-18 07:36:26 -0500

    Uploading the project's base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+function App() {
+  
+  return (
+    <>
+      <div className="container mx-auto mt-14">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <h1 className="font-black text-5xl text-zinc-600 text-center md:w-2/3 mx-auto">
+        Patients follow-up {""}
+        <span className="text-red-500">Veterinary</span>
+      </h1>
+    );
+  }
+  
+  export default Header;
+  
